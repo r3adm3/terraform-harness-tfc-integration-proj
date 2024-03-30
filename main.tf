5ddc97b6af90ffd6fa6f3438a7bafbd448b44918
Adrian Farnell <adrianjfarnell@gmail.com>
2024-03-30 19:11:32 +0000

    fix3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "harness_platform_project" "test_proj2" {
   identifier   = "test_proj2"
-  name         = "another test project doodad (tfc)"
+  name         = "another test project doodad tfc"
   org_id       = "default"
   color        = "#0063F7"
 }
