7b6bf7a34dd0daa8a139d609d3ac09bfddf0804f
Adrian Farnell <adrianjfarnell@gmail.com>
2024-03-30 19:07:57 +0000

    changed name of project to avoid clash with other repo

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "harness_platform_project" "test_proj" {
-  identifier   = "test_proj"
-  name         = "another test project doodad"
+  identifier   = "test_proj_tfc"
+  name         = "another test project doodad (tfc)"
   org_id       = "default"
   color        = "#0063F7"
 }
