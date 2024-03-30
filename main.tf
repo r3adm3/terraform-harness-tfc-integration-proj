da752c9863a8e2a86f1ea3b3fea1fc64d2d5cbd1
Adrian Farnell <adrianjfarnell@gmail.com>
2024-03-30 18:58:58 +0000

    added some actual code to deploy

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "harness_platform_project" "test_proj" {
+  identifier   = "test_proj"
+  name         = "another test project doodad"
+  org_id       = "default"
+  color        = "#0063F7"
+}
